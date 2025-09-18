c840bf9b8ff071d088c81e2c51e112be4da53258
CarrascoAlejandro <carrascoalejandro2003@gmail.com>
2025-09-18 08:47:05 -0400

    use case user management

--- src/main/java/bo/cirrus/demo/domain/port/in/UserManagementUseCase.java
+++ src/main/java/bo/cirrus/demo/domain/port/in/UserManagementUseCase.java
@@ -1,8 +1,12 @@
 package bo.cirrus.demo.domain.port.in;
 
+import bo.cirrus.demo.domain.model.User;
+
 public interface UserManagementUseCase {
     
-    // TODO: Define user management operations here for the following operations:
-    // - Create User
-    // - Find User by Username or Email (including their Privileges)
+    User createUser(CreateUserRequest request);
+
+    User findByUsernameOrEmail(String username, String email);
+
+    record CreateUserRequest(String username, String email, String password) { }
 }
